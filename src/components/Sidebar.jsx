ff8fa8196f754c51f76afe7740ed250f90ed8716
manash-codes <manash.codes@gmail.com>
2024-08-31 00:35:07 +0530

    Add Active Link to Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiOutlineProduct } from "react-icons/ai";
 import { MdOutlineCompare } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SidebarData = [
   {
@@ -16,13 +16,18 @@ const SidebarData = [
   },
 ];
 const Sidebar = () => {
+  const location = useLocation();
   return (
     <div className="p-4">
       <ul>
         {SidebarData.map((item, index) => (
           <Link
             to={item.path}
-            className="p-4 mb-1 flex items-center cursor-pointer hover:bg-gray-300 rounded-md dark:hover:bg-gray-700"
+            className={`p-4 mb-1 flex items-center cursor-pointer hover:bg-gray-300 rounded-md dark:hover:bg-gray-700
+              ${
+                location.pathname === item.path ? "text-gray-600" : "text-black"
+              }
+              `}
             key={index}
           >
             <div className="text-2xl">{item.icon}</div>
