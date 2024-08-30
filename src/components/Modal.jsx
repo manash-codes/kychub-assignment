2f99c0400ed41542ef0f336ed3b8e8547ea17c2a
manash-codes <manash.codes@gmail.com>
2024-08-31 00:42:56 +0530

    Update Modal Button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,8 +16,8 @@ const ModalComponent = () => {
   return (
     <>
       <Button
-        className="text-2xl p-2 hover:text-blue-300"
-        type="text"
+        className="text-4xl p-2 outline-none border-none hover:text-blue-300 absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        type="default"
         onClick={showModal}
       >
         <IoIosAddCircleOutline />
